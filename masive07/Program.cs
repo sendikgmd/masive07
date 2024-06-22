2f26727c1776925894315eda7b8708bd8f9188ea
sendikgmd <163888441+sendikgmd@users.noreply.github.com>
2024-06-22 12:39:12 +0300

    Добавьте файлы проекта.

---
+++ masive07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masive07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Это подход к программированию, который содержит ряд методов для задач разработки ПО";
+            string[] strings = str.Split(' ');
+            Console.WriteLine(str);
+            foreach (string s in strings)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
